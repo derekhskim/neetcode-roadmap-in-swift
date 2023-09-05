8ad33f65e848dc62205431e81ee01f6147af4706
[treasure3210] <treasure3210@gmail.com>
2023-09-04 23:08:50 -0600

    Solve problem with explanation

--- Valid Anagram/Valid Anagram.playground/Contents.swift
+++ Valid Anagram/Valid Anagram.playground/Contents.swift
@@ -16,6 +16,40 @@ import Foundation
 
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
+        // Very first, we can say `s` and `t` are not an anagram if the count doesn't match
+        if s.count != t.count {
+            return false
+        }
         
+        // Create a countTracker to count each Character in the string
+        var countTracker: [Character:Int] = [:]
+        
+        // iterate through String s and count each characters
+        for char in s {
+            if let count = countTracker[char] {
+                countTracker[char] = count + 1
+            } else {
+                countTracker[char] = 1
+            }
+        }
+        
+        // use available information to reduce count for characters that already exist
+        for char in t {
+            if let count = countTracker[char] {
+                // remove last character from the hash map
+                if count == 1 {
+                    countTracker.removeValue(forKey: char)
+                } else {
+                    // reduce counting if same character has been found and it's not the last one
+                    countTracker[char] = count - 1
+                }
+            } else {
+                // if the character in string t doesn't match with character in string s, return false
+                return false
+            }
+        }
+        
+        // if countTracker is empty, it's an anagram, therefore true, if it's not empty, it's not an anagram, therefore false
+        return countTracker.isEmpty
     }
 }
