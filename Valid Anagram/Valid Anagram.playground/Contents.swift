c62d928756ce9ff0ee7b161a45990147680c2c6d
[treasure3210] <treasure3210@gmail.com>
2023-09-04 22:58:41 -0600

    Initialize problem

---
+++ Valid Anagram/Valid Anagram.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/// Valid Anagram
+///
+/// Given two strings `s` and `t`, return `true` if `t` is an anagram of `s`, and `false` otherwise.
+///
+/// An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+///
+/// Example 1:
+///     Input: s = "anagram", t = "nagaram"
+///     Output: true
+///
+/// Example 2:
+///     Input: s = "rat", t = "car"
+///     Output: false
+
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        
+    }
+}
