e1fca7756152089910967f0e89baf0c6c13a8498
[treasure3210] <treasure3210@gmail.com>
2023-09-07 23:36:21 -0600

    solve problem with detailed explanation

--- Group Anagram/Group Anagram.playground/Contents.swift
+++ Group Anagram/Group Anagram.playground/Contents.swift
@@ -17,3 +17,32 @@ import Foundation
 /// Example 3:
 ///     Input: strs = ["a"]
 ///     Output: [["a"]]
+
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        // Create a strHash (a dictionary) to store anagrams with sorted string
+        // sorted string will be the key, and anagrams will be the value
+        var strHash: [String:[String]] = [:]
+        
+        // Iterate through all strings in input string array
+        for str in strs {
+
+            // Sort the characters of the str.
+            // This sorted string will act as key for our dictionary.
+            // Since anagrams will share same sorted string, this will help us group anagrams together
+            var sortedStr = String(str.sorted())
+        
+            // for each str, check if sorted string already exists.
+            // if it exists, append the original str to the array value associated with sortedStr
+            // if it doesn't exist, create a new key-value pair, key being sortedStr, value being anagram
+            if strHash[sortedStr] != nil {
+                strHash[sortedStr]?.append(str)
+            } else {
+                strHash[sortedStr] = [str]
+            }
+
+        }
+        // return the values of the dictionary strHash which are grouped anagrams.
+        return Array(strHash.values)
+    }
+}
