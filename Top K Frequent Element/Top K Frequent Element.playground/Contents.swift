196ce1e27db7a075268a92a0c957f5b74d65b036
[treasure3210] <treasure3210@gmail.com>
2023-09-08 23:02:28 -0600

    Solve problem with explanation

--- Top K Frequent Element/Top K Frequent Element.playground/Contents.swift
+++ Top K Frequent Element/Top K Frequent Element.playground/Contents.swift
@@ -11,3 +11,28 @@ import Foundation
 /// Example 2:
 ///     Input: nums = [1], k = 1
 ///     Output: [1]
+
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        // create a hash map
+        // keep track of the count to it's key
+        // return k amount of highest frequency
+        var emptyHash: [Int:Int] = [:]
+
+        // iterate through nums array using enumerated method
+        for (index, value) in nums.enumerated() {
+            // if the value is already inside hash map, increment count by 1
+            if let count = emptyHash[value] {
+                emptyHash[value] = count + 1
+            } else {
+                // if it doesn't exist (first sight), assign 1
+                emptyHash[value] = 1
+            }
+        }
+        
+        // sort the hash map by most frequent elements (other words, ones with the most counts to least count)
+        let frequentElements = emptyHash.sorted(by: { $0.value > $1.value }).prefix(k).map { $0.key }
+
+        return frequentElements
+    }
+}
