c8789823be5e93ed7e7f8a11f8fd2a3f425783ed
[treasure3210] <treasure3210@gmail.com>
2023-09-16 13:06:15 -0600

    solve problem with explanation

--- Longest Consecutive Sequence/Longest Consecutive Sequence.playground/Contents.swift
+++ Longest Consecutive Sequence/Longest Consecutive Sequence.playground/Contents.swift
@@ -13,3 +13,33 @@ import Foundation
 /// Example 2:
 ///     Input: nums = [0,3,7,2,5,8,4,6,0,1]
 ///     Output: 9
+
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        // Initialize hash set to remove all duplicates
+        let hashSet = Set(nums)
+
+        // Initialize maxStreak to keep track of the maximum consecutive sequence
+        var maxStreak = 0
+
+        // Iterate through all integer in hash set
+        for num in hashSet {
+            // If the value doesn't have element before it, it's not a consecutive sequence
+            if !hashSet.contains(num - 1) {
+                var currentNum = num
+                var currentStreak = 1
+
+                // If the value has an element after it, we start counting the streak
+                while hashSet.contains(currentNum + 1) {
+                    currentNum += 1
+                    currentStreak += 1
+                }
+
+                // Keep only the maximum among the maxStreak and currentStreak
+                maxStreak = max(maxStreak, currentStreak)
+            }
+        }
+
+        return maxStreak
+    }
+}
