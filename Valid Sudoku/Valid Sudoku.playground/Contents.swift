18dca0c76960722cc325538356e28d2d6867ed91
[treasure3210] <treasure3210@gmail.com>
2023-09-15 23:28:57 -0600

    Solve problem with explanation

--- Valid Sudoku/Valid Sudoku.playground/Contents.swift
+++ Valid Sudoku/Valid Sudoku.playground/Contents.swift
@@ -24,3 +24,38 @@ import Foundation
 ///     Input: board = [["8","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 ///     Output: false
 ///     Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+class Solution {
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        // initialize rows, columns, and boxes as Set of characters
+        var rows = Array(repeating: Set<Character>(), count: 9)
+        var columns = Array(repeating: Set<Character>(), count: 9)
+        var boxes = Array(repeating: Set<Character>(), count: 9)
+
+        // iterate through 2 dimension array
+        // nested for-loop will be necessary to iterate both rows and columns
+        for i in 0..<9 {
+            for j in 0..<9 {
+
+                let char = board[i][j]
+
+                if char == "." {
+                    continue
+                }
+
+                let boxIndex = (i / 3) * 3 + (j / 3)
+                
+                // if the value already exists, it's not valid.
+                if rows[i].contains(char) || columns[j].contains(char) || boxes[boxIndex].contains(char) {
+                    return false
+                }
+
+                rows[i].insert(char)
+                columns[j].insert(char)
+                boxes[boxIndex].insert(char)
+            }
+        }
+
+        return true
+    }
+}
