769c76f640fb6d67a5c757afee0a717e7100a31f
[treasure3210] <treasure3210@gmail.com>
2023-09-16 13:07:16 -0600

    add test cases

--- Longest Consecutive Sequence/Longest Consecutive Sequence.playground/Contents.swift
+++ Longest Consecutive Sequence/Longest Consecutive Sequence.playground/Contents.swift
@@ -43,3 +43,11 @@ class Solution {
         return maxStreak
     }
 }
+
+let solution = Solution()
+
+let nums = [100, 4, 200, 1, 3, 2]
+let nums2 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+
+solution.longestConsecutive(nums)
+solution.longestConsecutive(nums2)
