88ec1a640dc7e046daf94972f98bf326c46a5bb3
[treasure3210] <treasure3210@gmail.com>
2023-09-25 16:11:05 -0600

    Add test case

--- Two Sum II - Input Array Is Sorted/Two Sum II - Input Array Is Sorted.playground/Contents.swift
+++ Two Sum II - Input Array Is Sorted/Two Sum II - Input Array Is Sorted.playground/Contents.swift
@@ -46,3 +46,17 @@ class Solution {
         return [-1, -1]
     }
 }
+
+let solution = Solution()
+let numbers = [2, 7, 11, 15]
+let target = 9
+
+let numbers2 = [2, 3, 4]
+let target2 = 6
+
+let numbers3 = [-1, 0]
+let target3 = -1
+
+solution.twoSum(numbers, target)
+solution.twoSum(numbers2, target2)
+solution.twoSum(numbers3, target3)
